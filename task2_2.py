c514f700bc664b8dfdcdecd6a1e9a0f928220978
Haiwei Su <haiwei93@gmail.com>
2016-05-13 10:46:19 -0400

    new update

--- task2_2.py
+++ task2_2.py
@@ -1,58 +1,40 @@
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
-from config import roadtemp, road1, road2, road3, road4, road5, road6, road7
-from drawMatches import draw_matches
-import numpy as np
+from config import roadtemp, road1, road2, road3, road4
 
-MIN_MATCH_COUNT = 1
-
-img1 = cv2.imread(road7,0)          # queryImage
-img2 = cv2.imread(roadtemp, 0) # trainImage
+img1 = cv2.imread(roadtemp,0)          # queryImage
+img2 = cv2.imread(road1,0)             # trainImage
 
 # Initiate SIFT detector
-sift = cv2.SIFT()
+sift = cv2.xfeatures2d.SIFT_create()
 
 # find the keypoints and descriptors with SIFT
 kp1, des1 = sift.detectAndCompute(img1,None)
 kp2, des2 = sift.detectAndCompute(img2,None)
 
+# FLANN parameters
 FLANN_INDEX_KDTREE = 0
 index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-search_params = dict(checks = 50)
+search_params = dict(checks=50)   # or pass empty dictionary
 
-flann = cv2.FlannBasedMatcher(index_params, search_params)
+flann = cv2.FlannBasedMatcher(index_params,search_params)
 
 matches = flann.knnMatch(des1,des2,k=2)
 
-# store all the good matches as per Lowe's ratio test.
-good = []
-for m,n in matches:
+# Need to draw only good matches, so create a mask
+matchesMask = [[0,0] for i in xrange(len(matches))]
+
+# ratio test as per Lowe's paper
+for i,(m,n) in enumerate(matches):
     if m.distance < 0.7*n.distance:
-        good.append(m)
+        matchesMask[i]=[1,0]
 
-if len(good)>MIN_MATCH_COUNT:
-    src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
-    dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+draw_params = dict(matchColor = (0,255,0),
+                   singlePointColor = (255,0,0),
+                   matchesMask = matchesMask,
+                   flags = 0)
 
-    M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
-    matchesMask = mask.ravel().tolist()
+img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,matches,None,**draw_params)
 
-    h,w = img1.shape
-    pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
-    dst = cv2.perspectiveTransform(pts,M)
-
-    img2 = cv2.polylines(img2,[np.int32(dst)],True,255,3, cv2.LINE_AA)
-
-else:
-    print "Not enough matches are found - %d/%d" % (len(good),MIN_MATCH_COUNT)
-    matchesMask = None
-
-draw_params = dict(matchColor = (0,255,0), # draw matches in green color
-                   singlePointColor = None,
-                   matchesMask = matchesMask, # draw only inliers
-                   flags = 2)
-
-img3 = cv2.drawMatches(img1,kp1,img2,kp2,good,None,**draw_params)
-
-plt.imshow(img3, 'gray'),plt.show()
+plt.imshow(img3,),plt.show()
